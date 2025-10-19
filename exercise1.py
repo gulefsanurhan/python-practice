54cfdf57f73e7744a1ab482f0c2c1a9b05035218
Gülefşn Urhan <gulefsanurhn@gmail.com>
2025-10-19 22:35:03 +0300

    add exercise1

---
+++ exercise1.py
@@ -0,0 +1,15 @@
+name = input("Name: ")
+year_of_birth = input("Year of birth: ")
+year = input("Current year: ")
+
+try:
+    year_of_birth = int(year_of_birth)
+    year = int(year)
+    age = year - year_of_birth
+
+    if age < 0:
+        print("That can't be right, you aren't born yet.")
+    else:
+        print(f"Hello {name}, you are {age} years old!")
+except ValueError:
+    print("You should write integer numbers!")
